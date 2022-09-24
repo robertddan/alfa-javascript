cf755f7b7f2d129c36e9e8509c93c9a63890d227
Robert-Dan <danrobert445@gmail.com>
2022-09-24 13:08:43 +0000

    message

--- www/js/main.js
+++ www/js/main.js
@@ -1,12 +1,17 @@
 "use strict";
 
 class Chart {
-
+	
   svg;
 	xmlns;
-
+	
+	#height;
+	#width;
+	#grid_gap;
+	
 	constructor(sDivId) {
 		try {
+			if (!this.chart_setup()) throw 'chart_setup';
 			if (!this.chart_stage()) throw 'chart_stage';
 			if (!this.chart_limits()) throw 'chart_limits';
 			if (!this.chart_grid()) throw 'chart_grid';
@@ -15,13 +20,20 @@ class Chart {
 			console.error(e);
 		}
   }
+		
+	chart_setup() {
+		this.height = 420;
+		this.width = 600;
+		this.grid_gap = 30;
+		return true;
+  }
 	
 	chart_stage() {
 		this.xmlns = 'http://www.w3.org/2000/svg';
 		this.svg = document.createElementNS(this.xmlns, 'svg');
 		this.svg.setAttributeNS(null, 'id', 'chart');
-		this.svg.setAttributeNS(null, 'height', '400');
-		this.svg.setAttributeNS(null, 'width', '600');
+		this.svg.setAttributeNS(null, 'height', this.height);
+		this.svg.setAttributeNS(null, 'width', this.width);
 		return true;
   }
 	
@@ -33,14 +45,14 @@ class Chart {
 		borderVertical.setAttributeNS(null, 'x1', '0');
 		borderVertical.setAttributeNS(null, 'x2', '0');
 		borderVertical.setAttributeNS(null, 'y1', '0');
-		borderVertical.setAttributeNS(null, 'y2', '400');
+		borderVertical.setAttributeNS(null, 'y2', this.height);
 		borderVertical.setAttributeNS(null, 'stroke', 'Red');
 		borderVertical.setAttributeNS(null, 'stroke-widt', '2.75');
 		
 		linesHorizontal.setAttributeNS(null, 'x1', '0');
-		linesHorizontal.setAttributeNS(null, 'x2', '600');
-		linesHorizontal.setAttributeNS(null, 'y1', '400');
-		linesHorizontal.setAttributeNS(null, 'y2', '400');
+		linesHorizontal.setAttributeNS(null, 'x2', this.width);
+		linesHorizontal.setAttributeNS(null, 'y1', this.height);
+		linesHorizontal.setAttributeNS(null, 'y2', this.height);
 		linesHorizontal.setAttributeNS(null, 'stroke', 'Red');
 		
 		this.svg.appendChild(borderVertical);
@@ -50,31 +62,29 @@ class Chart {
 	}
 	
 	chart_grid() {
-		// grid
-		for (let i = 0; i < 20; i++) {
+		// grid horizontal
+		for (let i = 0; i < (this.width / this.grid_gap); i++) {
 			let gridHorizontal = document.createElementNS(this.xmlns, 'line');
-			gridHorizontal.setAttributeNS(null, 'x1', 0);
-			gridHorizontal.setAttributeNS(null, 'x2', 20 * i);
-			gridHorizontal.setAttributeNS(null, 'y1', 400);
-			gridHorizontal.setAttributeNS(null, 'y2', 20 * i);
+			gridHorizontal.setAttributeNS(null, 'x1', this.grid_gap * i);
+			gridHorizontal.setAttributeNS(null, 'y1', 0);
+			gridHorizontal.setAttributeNS(null, 'x2', this.grid_gap * i);
+			gridHorizontal.setAttributeNS(null, 'y2', this.height);
 			gridHorizontal.setAttributeNS(null, 'stroke', 'Gray');
 			this.svg.appendChild(gridHorizontal);
 		}
-		
+		// grid vertical
+		for (let i = 0; i < (this.height / this.grid_gap); i++) {
+			let gridVertical = document.createElementNS(this.xmlns, 'line');
+			gridVertical.setAttributeNS(null, 'x1', 0);
+			gridVertical.setAttributeNS(null, 'y1', this.grid_gap * i);
+			gridVertical.setAttributeNS(null, 'x2', this.width);
+			gridVertical.setAttributeNS(null, 'y2', this.grid_gap * i);
+			gridVertical.setAttributeNS(null, 'stroke', 'Gray');
+			this.svg.appendChild(gridVertical);
+		}
 		return true;
 	}
-
-/*
-<!-- chart boundaries -->
-{% for i in range(0, 26) %} # orizontal
-	<line x1="{{ 150 * i + 0 }}" y1="{{ 0 }}" x2="{{ 150 * i + 0 }}" y2="{{ 2200 }}" style="stroke:LightGray;stroke-width:1.75" /> <!-- vertical -->
-	{% if i <= 14 %} # vertical
-		<line x1="{{ 0 }}" y1="{{ 150 * i }}" x2="{{ 3900 }}" y2="{{ 150 * i }}" style="stroke:LightGray;stroke-width:1.75" /> <!-- orizontal -->
-	{% endif %}
-{% endfor %}
-<!-- chart boundaries -->
-*/
-
+	
 }
 
 document.addEventListener('DOMContentLoaded', () => {
