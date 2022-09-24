cc3550d18ce2b43cf47de3bcd195bdac0cece3bd
Robert-Dan <danrobert445@gmail.com>
2022-09-24 15:49:01 +0000

    message

--- www/js/main.js
+++ www/js/main.js
@@ -1,63 +1,5 @@
 'use strict';
 
-var prices = {
-	xhr: {},
-	list: {},
-	constructor: function(sDivId) {
-		try {
-			// get data
-			if (!this.prices_xhr()) throw 'prices_xhr';
-			if (!this.prices_get()) throw 'prices_get';
-			return true;
-		} catch (e) {
-			console.error(e);
-		}
-  },
-	get: function() {
-		console.log(this.list);
-		return this.list;
-	},
-	prices_xhr: function () {
-		if (window.XMLHttpRequest) {
-			// moderner Browser - IE ab version 7
-			this.xhr = new XMLHttpRequest();
-		} else if (window.ActiveXObject) {
-			// IE 6
-			this.xhr = new ActiveXObject('Microsoft.XMLHTTP');
-		}
-		return true;
-	},
-	prices_load: function(event) {
-		if(event.target.status != 200) throw event.target.status;
-		this.list = event.target.response;
-		console.log('Laden der Daten abgeschlossen');
-	},
-	prices_progress: function(event) {
-		console.log('Fortschritt beim Laden der Daten');
-	},
-	prices_abort: function(event) {
-		console.log('Laden der Daten abgebrochen');
-	},
-	prices_error: function(event) {
-		throw 'Fehler beim Laden der Daten aufgetretn';
-	},
-	prices_timeout: function(event) {
-		throw 'Timeout beim Laden der Daten aufgetreten';
-	},
-	prices_get: function() {
-		this.xhr.open('POST', 'script.php');
-		this.xhr.responseType = 'json';
-		this.xhr.addEventListener('load', this.prices_load);
-		this.xhr.addEventListener('progress', this.prices_progress);
-		this.xhr.addEventListener('abort', this.prices_abort);
-		this.xhr.addEventListener('error', this.prices_error);
-		this.xhr.addEventListener('timeout', this.prices_timeout);
-		this.xhr.send();
-		return true;
-  }
-};
-
-
 var chart = {
 	height: '',
 	width: '',
@@ -137,9 +79,74 @@ var chart = {
 	}
 };
 
-document.addEventListener('DOMContentLoaded', () => {
+var prices = {
+	xhr: '',
+	list: '',
+	constructor: function(sDivId) {
+		try {
+			// get data
+			console.log('constructor');
+			if (!this.prices_xhr()) throw 'prices_xhr';
+			if (!this.prices_get()) throw 'prices_get';
+			//return this.list;
+			return true;
+		} catch (e) {
+			console.error(e);
+		}
+  },
+	get: function() {
+		console.log('get');
+		console.log(this.list);
+		return this.list;
+	},
+	prices_xhr: function () {
+		console.log('prices_xhr');
+		if (window.XMLHttpRequest) {
+			// moderner Browser - IE ab version 7
+			this.xhr = new XMLHttpRequest();
+		} else if (window.ActiveXObject) {
+			// IE 6
+			this.xhr = new ActiveXObject('Microsoft.XMLHTTP');
+		}
+		return true;
+	},
+	prices_load: function(event) {
+		if(event.target.status != 200) throw event.target.status;
+		this.list = event.target.response;
+		console.log(this.list);
+		console.log('Laden der Daten abgeschlossen');
+	},
+	prices_progress: function(event) {
+		console.log('Fortschritt beim Laden der Daten');
+	},
+	prices_abort: function(event) {
+		console.log('Laden der Daten abgebrochen');
+	},
+	prices_error: function(event) {
+		throw 'Fehler beim Laden der Daten aufgetretn';
+	},
+	prices_timeout: function(event) {
+		throw 'Timeout beim Laden der Daten aufgetreten';
+	},
+	prices_get: function() {
+		console.log('prices_get');
+		this.xhr.open('POST', 'script.php');
+		this.xhr.responseType = 'json';
+		this.xhr.addEventListener('load', this.prices_load);
+		this.xhr.addEventListener('progress', this.prices_progress);
+		this.xhr.addEventListener('abort', this.prices_abort);
+		this.xhr.addEventListener('error', this.prices_error);
+		this.xhr.addEventListener('timeout', this.prices_timeout);
+		this.xhr.send();
+		return true;
+  }
+};
+
+document.addEventListener('DOMContentLoaded', async () => {
   console.log('DOM fully loaded and parsed');
 	if (!chart.constructor('wrapChart')) throw 'chart.constructor';
+	//console.log(prices.constructor());
 	if (prices.constructor()) console.log(prices.get());
 	else throw 'prices.constructor';
 });
+//console.log(prices.get());
