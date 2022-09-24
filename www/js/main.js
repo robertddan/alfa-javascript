ad7ad94af9e5862e84f4a84ea72d29be014c6e0e
Robert-Dan <danrobert445@gmail.com>
2022-09-24 12:52:43 +0000

    message

--- www/js/main.js
+++ www/js/main.js
@@ -1,208 +1,80 @@
 "use strict";
 
-
-//{
-
-/*
-var xmlns = "http://www.w3.org/2000/svg";
-
-var o_svg = document.createElementNS(xmlns, "svg");
-o_svg.setAttribute('height', '300');
-o_svg.setAttribute('width', '300');
-	
-var g = document.createElementNS(xmlns, "g");
-	
-// draw linear gradient
-var defs = document.createElementNS(xmlns, "defs");
-var grad = document.createElementNS(xmlns, "linearGradient");
-grad.setAttributeNS(null, "id", "gradient");
-grad.setAttributeNS(null, "x1", "0%");
-grad.setAttributeNS(null, "x2", "0%");
-grad.setAttributeNS(null, "y1", "100%");
-grad.setAttributeNS(null, "y2", "0%");
-var stopTop = document.createElementNS(xmlns, "stop");
-stopTop.setAttributeNS(null, "offset", "0%");
-stopTop.setAttributeNS(null, "stop-color", "#ff0000");
-grad.appendChild(stopTop);
-var stopBottom = document.createElementNS(xmlns, "stop");
-stopBottom.setAttributeNS(null, "offset", "100%");
-stopBottom.setAttributeNS(null, "stop-color", "#0000ff");
-grad.appendChild(stopBottom);
-defs.appendChild(grad);
-g.appendChild(defs);
-	
-    // draw borders
-    var coords = "M 0, 0";
-    coords += " l 0, 300";
-    coords += " l 300, 0";
-    coords += " l 0, -300";
-    coords += " l -300, 0";
-
-
-	//<line x1="0" y1="0" x2="200" y2="200" style="stroke:rgb(255,0,0);stroke-width:2" />
-	//<line x1="0" y1="80" x2="100" y2="20" stroke="black" />
-	
-    var line = document.createElementNS(xmlns, "line");
-    line.setAttributeNS(null, 'x1', "0");
-    line.setAttributeNS(null, 'y1', "80");
-    line.setAttributeNS(null, 'x2', "100");
-    line.setAttributeNS(null, 'y2', "20");
-    line.setAttributeNS(null, 'stroke', "black");
-    line.setAttributeNS(null, 'stroke-width', "2");
-    g.appendChild(line);
-	
-o_svg.appendChild(g);
-var svgContainer = document.getElementById("wrapChart");
-svgContainer.appendChild(o_svg);
-*/
-	
-/*
-let unorderedList = document.querySelector("ul");
-console.log(unorderedList);
-let listItem = document.createElement("li");
-listItem.textContent = 4;
-listItem.className = "newItem";
-console.log(listItem);
-console.log(unorderedList.appendChild(listItem));
-*/
-	
-/*
-!!! GOOOOD
-	let sDivId = 'wrapChart';
-	
-	var xmlns = "http://www.w3.org/2000/svg";
-	// let o_svg = document.querySelector('svg');
-	var o_svg = document.createElementNS(xmlns, "svg");
-	o_svg.setAttributeNS(null, 'height', '210');
-	o_svg.setAttributeNS(null, 'width', '500');
-	o_svg.style.display = "block";
-	
-	//let linesVertical = document.createElement('line');
-	var linesVertical = document.createElementNS(xmlns, "line");
-	linesVertical.setAttributeNS(null, 'x1', '0');
-	linesVertical.setAttributeNS(null, 'x2', '200');
-	linesVertical.setAttributeNS(null, 'y1', '0');
-	linesVertical.setAttributeNS(null, 'y2', '200');
-	linesVertical.setAttributeNS(null, 'stroke', 'Red');
-	linesVertical.setAttributeNS(null, 'stroke-widt', '2.75');
-	
-	o_svg.appendChild(linesVertical);
-	const container = document.getElementById(sDivId);
-	container.append(o_svg);
-	
-	console.log(container.childNodes) // NodeList [ <p> ]
-*/
-	
-/*
-<svg height="210" width="500">
-  <line x1="0" y1="0" x2="200" y2="200" style="stroke:rgb(255,0,0);stroke-width:2" />
-</svg> 
-*/
-	
-/*
-	let sDivId = 'wrapChart';
-	let svg = document.createElement('svg');
-	//let svg = document.querySelector('svg');
-	svg.setAttribute('id', 'chart');
-	svg.setAttribute('height', '400');
-	svg.setAttribute('width', '630');
-	svg.setAttribute('style', 'background-color: beige;');
-	
-
-	// boundaries
-	let linesVertical = document.createElement('line');
-	let linesHorizontal = document.createElement('line');
-
-	linesVertical.setAttribute('x1', '0');
-	linesVertical.setAttribute('x2', '0');
-	linesVertical.setAttribute('y1', '0');
-	linesVertical.setAttribute('y2', '400');
-	linesVertical.setAttribute('stroke', 'Red');
-	linesVertical.setAttribute('stroke-widt', '2.75');
-
-	linesHorizontal.setAttribute('x1', '0');
-	linesHorizontal.setAttribute('x2', '600');
-	linesHorizontal.setAttribute('y1', '600');
-	linesHorizontal.setAttribute('y2', '600');
-	linesVertical.setAttribute('stroke', 'Red');
-	linesVertical.setAttribute('stroke-widt', '2.75');
-
-//<line x1="0" y1="0" x2="0" y2="2600" style="stroke:Red;stroke-width:2.75" /> <!-- vertical -->
-//<line x1="0" y1="2600" x2="2600" y2="2600" style="stroke:Red;stroke-width:2.75"/> <!-- orizontal -->
-
-	svg.appendChild(linesVertical);
-	svg.appendChild(linesHorizontal);
-
-	const container = document.getElementById(sDivId);
-	container.appendChild(svg);
-*/
-
-//}
-
-/*
-	let sDivId = 'wrapChart';
-	
-	var xmlns = "http://www.w3.org/2000/svg";
-	// let o_svg = document.querySelector('svg');
-	var o_svg = document.createElementNS(xmlns, "svg");
-	o_svg.setAttributeNS(null, 'height', '210');
-	o_svg.setAttributeNS(null, 'width', '500');
-	o_svg.style.display = "block";
-	
-	//let linesVertical = document.createElement('line');
-	var linesVertical = document.createElementNS(xmlns, "line");
-	linesVertical.setAttributeNS(null, 'x1', '0');
-	linesVertical.setAttributeNS(null, 'x2', '200');
-	linesVertical.setAttributeNS(null, 'y1', '0');
-	linesVertical.setAttributeNS(null, 'y2', '200');
-	linesVertical.setAttributeNS(null, 'stroke', 'Red');
-	linesVertical.setAttributeNS(null, 'stroke-widt', '2.75');
-	
-	o_svg.appendChild(linesVertical);
-	const container = document.getElementById(sDivId);
-	container.append(o_svg);
-	
-	console.log(container.childNodes) // NodeList [ <p> ]
-*/
-
 class Chart {
 
   svg;
 	xmlns;
 
 	constructor(sDivId) {
+		try {
+			if (!this.chart_stage()) throw 'chart_stage';
+			if (!this.chart_limits()) throw 'chart_limits';
+			if (!this.chart_grid()) throw 'chart_grid';
+			document.getElementById(sDivId).appendChild(this.svg);
+		} catch (e) {
+			console.error(e);
+		}
+  }
+	
+	chart_stage() {
 		this.xmlns = 'http://www.w3.org/2000/svg';
 		this.svg = document.createElementNS(this.xmlns, 'svg');
 		this.svg.setAttributeNS(null, 'id', 'chart');
 		this.svg.setAttributeNS(null, 'height', '400');
-		this.svg.setAttributeNS(null, 'width', '630');
-		
-		// boundaries
-		let linesVertical = document.createElementNS(this.xmlns, 'line');
+		this.svg.setAttributeNS(null, 'width', '600');
+		return true;
+  }
+	
+	chart_limits() {
+		// limits
+		let borderVertical = document.createElementNS(this.xmlns, 'line');
 		let linesHorizontal = document.createElementNS(this.xmlns, 'line');
 		
-		linesVertical.setAttributeNS(null, 'x1', '0');
-		linesVertical.setAttributeNS(null, 'x2', '0');
-		linesVertical.setAttributeNS(null, 'y1', '0');
-		linesVertical.setAttributeNS(null, 'y2', '400');
-		linesVertical.setAttributeNS(null, 'stroke', 'Red');
-		linesVertical.setAttributeNS(null, 'stroke-widt', '2.75');
+		borderVertical.setAttributeNS(null, 'x1', '0');
+		borderVertical.setAttributeNS(null, 'x2', '0');
+		borderVertical.setAttributeNS(null, 'y1', '0');
+		borderVertical.setAttributeNS(null, 'y2', '400');
+		borderVertical.setAttributeNS(null, 'stroke', 'Red');
+		borderVertical.setAttributeNS(null, 'stroke-widt', '2.75');
 		
 		linesHorizontal.setAttributeNS(null, 'x1', '0');
 		linesHorizontal.setAttributeNS(null, 'x2', '600');
-		linesHorizontal.setAttributeNS(null, 'y1', '600');
-		linesHorizontal.setAttributeNS(null, 'y2', '600');
+		linesHorizontal.setAttributeNS(null, 'y1', '400');
+		linesHorizontal.setAttributeNS(null, 'y2', '400');
 		linesHorizontal.setAttributeNS(null, 'stroke', 'Red');
-		linesHorizontal.setAttributeNS(null, 'stroke-widt', '2.75');
 		
-//<line x1="0" y1="0" x2="0" y2="2600" style="stroke:Red;stroke-width:2.75" /> <!-- vertical -->
-//<line x1="0" y1="2600" x2="2600" y2="2600" style="stroke:Red;stroke-width:2.75"/> <!-- orizontal -->
-
-		this.svg.appendChild(linesVertical);
+		this.svg.appendChild(borderVertical);
 		this.svg.appendChild(linesHorizontal);
 		
-		document.getElementById(sDivId).appendChild(this.svg);
-  }
+		return true;
+	}
+	
+	chart_grid() {
+		// grid
+		for (let i = 0; i < 20; i++) {
+			let gridHorizontal = document.createElementNS(this.xmlns, 'line');
+			gridHorizontal.setAttributeNS(null, 'x1', 0);
+			gridHorizontal.setAttributeNS(null, 'x2', 20 * i);
+			gridHorizontal.setAttributeNS(null, 'y1', 400);
+			gridHorizontal.setAttributeNS(null, 'y2', 20 * i);
+			gridHorizontal.setAttributeNS(null, 'stroke', 'Gray');
+			this.svg.appendChild(gridHorizontal);
+		}
+		
+		return true;
+	}
+
+/*
+<!-- chart boundaries -->
+{% for i in range(0, 26) %} # orizontal
+	<line x1="{{ 150 * i + 0 }}" y1="{{ 0 }}" x2="{{ 150 * i + 0 }}" y2="{{ 2200 }}" style="stroke:LightGray;stroke-width:1.75" /> <!-- vertical -->
+	{% if i <= 14 %} # vertical
+		<line x1="{{ 0 }}" y1="{{ 150 * i }}" x2="{{ 3900 }}" y2="{{ 150 * i }}" style="stroke:LightGray;stroke-width:1.75" /> <!-- orizontal -->
+	{% endif %}
+{% endfor %}
+<!-- chart boundaries -->
+*/
+
 }
 
 document.addEventListener('DOMContentLoaded', () => {
