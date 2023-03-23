c9444925bcca8d14ace3d5072eb01f9b4faaa9c7
Robert-Dan <danrobert445@gmail.com>
2023-03-23 17:26:42 +0000

    chart fix

--- sh_git.sh
+++ sh_git.sh
@@ -8,7 +8,8 @@ git status
 #varname=${varname:-message} #📋📋📋✏️✏️✏️📜📜📜} # 👌👌👌🙏🙏🙏✌️✌️✌️
 
 git commit -m "chart fix" 
-git remote add origin https://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@github.com/johnidevo/Alfatraining_javascript.git
+#git remote add origin https://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@github.com/johnidevo/Alfatraining_javascript.git
+git remote add origin https://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@github.com/robertddan/alfa-javascript.git
 git push origin master
 
 
